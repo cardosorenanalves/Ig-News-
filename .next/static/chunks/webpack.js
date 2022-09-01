c13c8f3ad20eceac81a1df54c5da77766e2a4db7
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-01 12:20:20 -0300

    Feat: Home page create

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "879f414d2fc22c89"; }
+/******/ 		__webpack_require__.h = function() { return "9c965b2558b30312"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,18 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
    </>
   )
 }
