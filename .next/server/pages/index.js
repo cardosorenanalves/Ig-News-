67755a2da928da7a1337e830d5be2007deab34c6
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-02 11:30:12 -0300

    Chore: add next-auth and update tsconfig

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -115,7 +115,7 @@ module.exports = require("stripe");
 /***/ ((module) => {
 
 "use strict";
-module.exports = JSON.parse('{"name":"ignews","version":"0.1.0","private":true,"scripts":{"dev":"next dev","build":"next build","start":"next start","lint":"next lint"},"dependencies":{"next":"12.2.0","react":"18.2.0","react-dom":"18.2.0","react-icons":"^4.4.0","sass":"^1.53.0","stripe":"^10.7.0"},"devDependencies":{"@types/node":"^18.0.3","@types/react":"^18.0.15","eslint":"8.19.0","eslint-config-next":"12.2.0","typescript":"^4.7.4"}}');
+module.exports = JSON.parse('{"name":"ignews","version":"0.1.0","private":true,"scripts":{"dev":"next dev","build":"next build","start":"next start","lint":"next lint"},"dependencies":{"next":"12.2.0","next-auth":"^4.10.3","react":"18.2.0","react-dom":"18.2.0","react-icons":"^4.4.0","sass":"^1.53.0","stripe":"^10.7.0"},"devDependencies":{"@types/node":"^18.0.3","@types/react":"^18.0.15","eslint":"8.19.0","eslint-config-next":"12.2.0","typescript":"^4.7.4"}}');
 
 /***/ })
 
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a3448244b1816c2"; }
+/******/ 		__webpack_require__.h = function() { return "5226376efb02cf98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
