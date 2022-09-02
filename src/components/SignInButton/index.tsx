44a8ea8f5619a8ec54bd07f95d77ca75afbeff3b
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-02 11:29:06 -0300

    Refactor: exchanging static information for dynamic github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,33 @@
 import  { FaGithub } from '../../../node_modules/react-icons/fa/index'
 import  { FiX } from '../../../node_modules/react-icons/fi/index'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 
 
 import Styles from "./styles.module.scss";
 
 export function SignInButton(){
-const isUserLoggedIn = false;
+const {data: session} = useSession()
+
+console.log(session)
 
 
-return isUserLoggedIn ?(
+return session ?(
     <button 
     type="button"
    className={Styles.SignInButton} 
+   onClick={()=> signOut()}
    >
 
         <FaGithub color='#04d361'/>
-        Renan Cardoso
+        {session.user.name}
         <FiX color='#737380' className={Styles.closeIcon}/>
     </button>
 ) : (
     <button 
     type="button"
    className={Styles.SignInButton} 
+   onClick={()=> signIn('github')}
    >
 
         <FaGithub color='#eba417'/>
