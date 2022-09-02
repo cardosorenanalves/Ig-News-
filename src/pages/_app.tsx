744a68b7e908f233fa8f7fc09b7c482b2a8beefd
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-02 11:28:11 -0300

    Feat: using application module in the app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import { AppProps } from 'next/app'
 import { Header } from '../components/Header/index'
+import { SessionProvider } from 'next-auth/react'
+
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps } ) {
+function MyApp({ Component, pageProps }: AppProps ) {
 
   return(
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header/>
       <Component {...pageProps} />
-   </>
+   </SessionProvider>
   )
  
 
