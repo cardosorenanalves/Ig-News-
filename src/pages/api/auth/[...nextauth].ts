d4a76d5af160a9d835ebcd122635f05d9a4896e2
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-05 15:42:33 -0300

    Refactor: callback signin refactored

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,34 +22,21 @@ export default NextAuth({
       ],
 
       callbacks: {
-        async signIn({user, account, profile}) {
-            const {email} = user
-            try{
+        async signIn({user}) {
+          const { email } = user
+
+          try {
             await fauna.query(
-                q.If(
-                  q.Not(
-                    q.Exists(
-                      q.Match(
-                        q.Index('user_by_email'),
-                        q.Casefold(user.email)
-                      )
-                    )
-                  ),
-                  q.Create(
-                    q.Collection('users'),
-                    {data: {email}}
+              q.If(
+                q.Not(
+                  q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
                 ),
-                q.Get(
-                  q.Match(
-                    q.Index('user_by_email'),
-                    q.Casefold(user.email)
-                  )
-                )
-                )
+                q.Create(q.Collection('users'), { data: { email } }),
+                q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+              )
             )
-
+    
             return true
-
           } catch {
             return false
           }
