c128ff26ab8252ada4e233f7a1b41479efa66b05
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-05 10:01:17 -0300

    Feat: function for checkout method in stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -5,11 +6,20 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps){
+    const {data: session} = useSession()
+    
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+    }
 
     return(
        <button
        type="button"
        className={styles.subscribeButton}
+       onClick={handleSubscribe}
        >
         Subscribe now
        </button> 
