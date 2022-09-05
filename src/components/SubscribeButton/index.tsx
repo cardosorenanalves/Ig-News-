a8aebe4f2d11fabc5dda9789ab77da441d6880c9
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-05 15:39:37 -0300

    Chore: stripe-js and axios add

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -8,10 +10,22 @@ interface SubscribeButtonProps{
 export function SubscribeButton({priceId}: SubscribeButtonProps){
     const {data: session} = useSession()
     
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
             signIn('github')
             return;
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            alert(err.message)
         }
     }
 
