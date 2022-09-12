4546f6cddd405d4cca313dc32375c4b639a7da5f
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-12 14:38:05 -0300

    Refactor: component ActiveLink utilized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton/index'
 import styles from './styles.module.scss'
 
 export function Header(){
+   
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                <img src="/images/logo.svg" alt="ig.news" /> 
                <nav>
-                   <a className={styles.active}>Home</a>
-                   <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                </nav>
 
                <SignInButton />
