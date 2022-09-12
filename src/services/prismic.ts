c4eb2493eef33bbbd4666f23b8b9c3242ef067db
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-12 12:37:08 -0300

    Refactor: process.env modified

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client'
 
 export function getPrismicClient(){
     const prismic = Prismic.createClient(
-        process.env.PRISMIC_ACCESS_TOKEN,
+        process.env.PRISMIC_ENDPOINT,
         {
             
             accessToken: process.env.PRISMIC_ACCESS_TOKEN
