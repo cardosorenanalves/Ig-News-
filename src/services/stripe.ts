5279610ffc12bc32df768381b7eb4a4af95d2997
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-12 12:33:44 -0300

    Refactor: Stripe services modified

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,13 +1,13 @@
 import Stripe from "stripe";
-import { version } from '../../package.json'
+
 
 export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY!,
+    process.env.STRIPE_API_KEY,
     {
         apiVersion: '2022-08-01',
         appInfo:{
             name: 'Ignews',
-            version
+            version: '0.1.0'
         },
 }
 )
