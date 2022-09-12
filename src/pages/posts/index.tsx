4f46e72f178763fc85dd436e7d80d5fc0d50f674
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-12 14:40:13 -0300

    Feat: utilize of prismic content

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,21 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './style.module.scss'
-import Prismic from '@prismicio/client'
+import * as Prismic from '@prismicio/client'
+import {RichText} from 'prismic-dom'
+import Link from 'next/link';
 
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+interface PostsProps{
+    posts: Post[]
+}
 
-export default function Posts(){
+export default function Posts({posts}: PostsProps){
     return(
         <>
             <Head>
@@ -14,21 +25,16 @@ export default function Posts(){
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href='#'>
-                        <time>12 de setembro de 2022</time>
-                        <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong>
-                        <p>Harum explicabo magni rerum ducimus possimus maiores neque voluptates! Ea eveniet sunt facere maiores eligendi consequuntur eum quam amet, adipisci, quidem officia?</p>
-                    </a>  
-                    <a href='#'>
-                        <time>12 de setembro de 2022</time>
-                        <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong>
-                        <p>Harum explicabo magni rerum ducimus possimus maiores neque voluptates! Ea eveniet sunt facere maiores eligendi consequuntur eum quam amet, adipisci, quidem officia?</p>
-                    </a>  
-                    <a href='#'>
-                        <time>12 de setembro de 2022</time>
-                        <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong>
-                        <p>Harum explicabo magni rerum ducimus possimus maiores neque voluptates! Ea eveniet sunt facere maiores eligendi consequuntur eum quam amet, adipisci, quidem officia?</p>
-                    </a>  
+                    {posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                          <a key={post.slug} href='#'>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>  
+                        </Link>
+                      
+                    ))}
                 </div>
             </main>
         </>
@@ -38,14 +44,26 @@ export default function Posts(){
 export const getStaticProps: GetStaticProps = async () =>{
     const prismic = getPrismicClient()
 
-    const response = await prismic.getByType(
-        Prismic.predicate.at('document.type', 'publication')
-    , {
-        fetch: ['publication.title', 'publication.content'],
-        pageSize: 100,
+    const response = await prismic.getByType( 'publication', {
+        lang: 'pt-BR',
+    });
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
     })
 
     return{
-        props: {}
+        props: {
+            posts
+        }
     }
 }
